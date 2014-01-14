f7772e68e057b0d2c4538027a8f1c6d1ef2ab09d
Emmanuel Odeke <odeke@ualberta.ca>
2014-01-13 21:42:15 -0700

    Method to get maxInt size and programmatically add that to models.py

--- thebear/theBearConstants.py
+++ thebear/theBearConstants.py
@@ -1,3 +1,4 @@
+# Author: Emmanuel Odeke <odeke@ualberta.ca>
 # Constants for thebear
 
 # Table string names
@@ -5,8 +6,19 @@ SONG_TABLE_KEY = "Song"
 PLAYTIME_TABLE_KEY = "PlayTime"
 SONGENTRY_TABLE_KEY = "SongEntry"
 
+# Function to determine bit length of unsigned ints
+# Enable some tail recursion
+bitLength = lambda i,l=1 : l if i <= 1 else bitLength(i>>1, l+1)
+
+import sys
+pyVersion = sys.hexversion/(1<<24)
+MAX_INT = sys.maxint if pyVersion < 3 else sys.maxsize
+
 # Max values
-MAX_DIGITS = 1<<15 # Arbitrary value
+MAX_DIGITS = bitLength(MAX_INT)
+assert(MAX_DIGITS >= 1) # Sanity check
+
+MAX_TIME_DIGITS = MAX_DIGITS
 MAX_HASH_LENGTH = 400 #Arbitrary value here
 MAX_DECIMAL_PLACES = 5 # Arbitrarily
 MAX_MISC_STR_LENGTH = 200
