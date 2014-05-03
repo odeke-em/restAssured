b6428cc91b1fbed2444ca33668700b7b60885b38
Emmanuel Odeke <emmanuel.odeke@cmgcanada.ca>
2014-05-03 01:07:17 -0400

    Proper url pattern

--- chatServer/urls.py
+++ chatServer/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^/', views.messageHandler, name='messageHandler'),
+    url(r'^[/]?', views.messageHandler, name='messageHandler'),
     url(r'^messageHandler', views.messageHandler, name='messageHandler'),
     url(r'^receipientHandler', views.receipientHandler, name='receipientHandler'),
     url(r'^messageMarkerHandler', views.messageMarkerHandler, name='messageMarkerHandler'),
